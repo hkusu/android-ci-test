bbb7f5d399db6e101c7932ac9fb2abbafd8fa73a
Hiroyuki Kusu <kusu0806@gmail.com>
2024-03-19 12:02:29 +0900

    JUnit のテスト push

--- shared/src/commonMain/kotlin/com/example/myhandsonapplication/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/myhandsonapplication/Greeting.kt
@@ -4,6 +4,6 @@ class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "HelloAAA, ${platform.name}!"
+        return "Hell, ${platform.name}!"
     }
 }
